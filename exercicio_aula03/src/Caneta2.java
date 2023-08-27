76d6e279ce61db22c18e6c9e5453977c563480c2
Amanda Rodrigues <amanda.study110@gmail.com>
2023-08-26 21:58:25 -0300

    java
    metodos get e setters

---
+++ exercicio_aula03/src/Caneta2.java
@@ -0,0 +1,9 @@
+public class Caneta2 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarela",0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("CST", "Laranja", 1.5f);
+        c2.status();
+
+    }
+}
