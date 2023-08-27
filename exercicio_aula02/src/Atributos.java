53feecf9d90c189b935ff8970536d15af97fa1af
Amanda Rodrigues <amanda.study110@gmail.com>
2023-08-26 21:20:31 -0300

    java
    private, public and restricted

---
+++ exercicio_aula02/src/Atributos.java
@@ -0,0 +1,9 @@
+public class Atributos {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic Cristal";
+        c1.cor = "Azul";
+        c1.status();
+    }
+}
+
---
+++ exercicio_aula02/src/Caneta.java
@@ -0,0 +1,29 @@
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+   public void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("ERRO, não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+    }
+
+    protected void tampar(){
+        this.tampada = true;
+    }
+    protected void destampar(){
+        this.tampada = false;
+    }
+}
